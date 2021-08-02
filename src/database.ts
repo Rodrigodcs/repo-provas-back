1b7d50af14af633269eecae6b569ed7f2298bc64
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-08-02 14:25:27 -0300

    deploy 3

--- src/database.ts
+++ src/database.ts
@@ -11,11 +11,7 @@ export default async function connect () {
     type: "postgres",
     url: process.env.DATABASE_URL,
     entities: [`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.*`],
-    extra: {
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
+    ssl: process.env.NODE_ENV === 'production'
   });
   await connection.connect();
   return connection;
